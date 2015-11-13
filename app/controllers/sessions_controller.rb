df09029d7028fb186badcb124c4dddbdd8d80538
Jiyoon Kim & Tahrim Uddin <pair+jkim97.utahrim@devbootcamp.com>
2015-11-13 11:24:28 -0500

    changed session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
-    redirect_to questions_path
+    redirect_to root_path
   end
 
   def new
@@ -10,12 +10,12 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(username: params[:user][:username])
-    if user.password == params[:user][:password]
-      session[:user_id] = user.user_id
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
 
-      redirect_to questions_path
+      redirect_to root_path
     else
-      redirect_to login_path
+      render :new
     end
   end
 end
