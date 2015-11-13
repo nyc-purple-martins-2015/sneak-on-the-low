fe0bf899ca84a593080e96ee41be8cb18de8c04b
Jiyoon Kim & Tahrim Uddin <pair+jkim97.utahrim@devbootcamp.com>
2015-11-13 16:19:22 -0500

    modified answer controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,3 +1,20 @@
 class AnswersController < ApplicationController
+  def new
+    @answer = current_user.answers.new
+  end
 
+  def create
+    @answer = current_users.answers.new(answer_params)
+    if @answer.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def answer_params
+    answer_params = params.require(:answer).permit(:content)
+  end
 end
