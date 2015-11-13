cec4d4a73336df068ab7522c59cfcd0c6e318b88
Jiyoon Kim & Tahrim Uddin <pair+jkim97.utahrim@devbootcamp.com>
2015-11-13 11:33:52 -0500

    made change for session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,8 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
-  def new
-    @user = User.new
-  end
-
   def create
-    user = User.find_by(username: params[:user][:username])
+    user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
 
