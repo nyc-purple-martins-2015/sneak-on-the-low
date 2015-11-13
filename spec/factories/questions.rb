86fbf4740d75eae17e6cf9a6170c6eac74c14efb
Nick Perri <nperri6@gmail.com>
2015-11-13 11:59:27 -0500

    add image and some rspec

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
+
   factory :question do
-    
+    question = user.questions.create(title: Faker::Book.title, content: Faker::Lorem.paragraph(1))
+    title Faker::Book.title
+    content Faker::Lorem.paragraph(1)
+    author User.create(username: "nick", email: "nick@gmail.com", password: "password")
   end
 
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    it { should validate_presence_of :title}
+    it { should validate_presence_of :content}
+    it { should validate_presence_of :author}
+  end
+
+  context "#question" do
+    let(:question) {FactoryGirl.build :question}
+  end
 end
