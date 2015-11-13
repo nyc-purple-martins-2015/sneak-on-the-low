829ed6d2875579f41ced78d6ec2439e9962d814d
Jiyoon Kim & Tahrim Uddin <pair+jkim97.utahrim@devbootcamp.com>
2015-11-13 12:29:15 -0500

    added test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,10 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context "validations" do
     it { should validate_presence_of :username }
+    it { should allow_value('tahrim').for(:username)}
+    it { should validate_presence_of :email }
+    it { should allow_value('test@test.com').for(:email) }
+    it { should_not allow_value('badlyFormattedEmail').for(:email) }
   end
+
 end
