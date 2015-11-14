b71082da34ed7348e0a8c5a88f7dae1e010969b8
Nick Perri <nperri6@gmail.com>
2015-11-14 14:11:29 -0500

    add ability to add comment to a question

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def current_user
     @current_user ||= User.find(session[:user_id]) if logged_in?
   end
+
+  def question_owner?(question)
+    current_user == question.author
+  end
+
+  def format_time(object)
+    localize(object.updated_at, :format => :long)
+  end
 end
