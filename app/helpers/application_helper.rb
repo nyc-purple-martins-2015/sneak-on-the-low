362189fea442be4a4836e74fd1ae154f149b4663
Jiyoon Kim & Tahrim Uddin <pair+jkim97.utahrim@devbootcamp.com>
2015-11-13 19:07:21 -0500

    change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id])
+    @current_user ||= User.find(session[:user_id]) if logged_in?
   end
 end
