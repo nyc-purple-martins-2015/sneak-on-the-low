e9c89515cd9bd6cb9a350bb6f554435e7afd75b0
André <email": null>
2015-11-14 18:10:58 -0500

    add answer delete

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -28,7 +28,10 @@ class AnswersController < ApplicationController
   end
 
   def destroy
-
+    @answer = Answer.find(params[:id])
+    @question = @answer.question
+    @answer.destroy
+    redirect_to question_path(@question)
   end
 
   private
