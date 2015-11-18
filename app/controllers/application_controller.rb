86e8186fb8c87669a43e6c41034ce33b9f0a5794
Steven Cassidy <steven.cassidy@gmail.com>
2015-11-17 21:44:44 -0500

    Fix logged in to return true only if session[:user_id] is a valid user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::Base
   helper_method :logged_in?, :current_user
 
   def logged_in?
-    !!session[:user_id]
+    !!current_user
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if logged_in?
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
 
